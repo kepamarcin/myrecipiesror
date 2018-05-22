9322b8ade71b0af0a0b80df84105c961bc1909b9
mkepa <pinmar1980@outlook.com>
2018-05-22 16:58:29 +0000

    chef model with tests

---
+++ db/migrate/20180506084043_change_column_in_recipies.rb
@@ -0,0 +1,6 @@
+class ChangeColumnInRecipies < ActiveRecord::Migration[5.0]
+  def change
+    rename_column :recipes, :email, :description
+    change_column :recipes, :description, :text
+  end
+end
